a1d2586a55c04eed297e42971d7c372f94fdde96
Marat Gumerov <marat.gumerof@aspose.com>
2020-02-27 18:56:14 +0400

    Deriver type property support added

--- Internal/SerializationHelper.cs
+++ Internal/SerializationHelper.cs
@@ -91,11 +91,14 @@ namespace Aspose.Email.Cloud.Sdk
         private class JsonTypeDeriveConverter : JsonConverter
         {
             private const string TypeProperty = "Type";
+            private const string DerivedTypeProperty = "DerivedType";
             private static Dictionary<string, Type> models;
 
             public override bool CanConvert(Type objectType)
             {
-                return objectType.GetProperty(TypeProperty) != null;
+                return 
+                    objectType.GetProperty(TypeProperty) != null ||
+                    objectType.GetProperty(DerivedTypeProperty) != null;
             }
 
             public override object ReadJson(
@@ -129,7 +132,8 @@ namespace Aspose.Email.Cloud.Sdk
                 var derivedTypeName = string.Empty;
                 foreach (var property in properties)
                 {
-                    if (!property.Name.Equals(TypeProperty, StringComparison.InvariantCultureIgnoreCase))
+                    if (!property.Name.Equals(TypeProperty, StringComparison.InvariantCultureIgnoreCase) ||
+                        !property.Name.Equals(DerivedTypeProperty, StringComparison.InvariantCultureIgnoreCase))
                     {
                         continue;
                     }
