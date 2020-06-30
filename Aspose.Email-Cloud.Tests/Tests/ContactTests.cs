520a7496bc435a9d0b69178cc5e4fac861b9cb8b
Marat Gumerov <marat.gumerof@aspose.com>
2020-07-01 01:03:24 +0400

    Added tests for MapiContact and MapiCalendar DTOs

---
+++ Aspose.Email-Cloud.Tests/Tests/ContactTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.Email.Cloud.Sdk.Model;
+using Aspose.Email.Cloud.Sdk.Model.Requests;
+using Aspose.Email.Cloud.Sdk.Tests.Utils;
+using NUnit.Framework;
+
+namespace Aspose.Email.Cloud.Sdk.Tests.Tests
+{
+    [Pipeline]
+    public class ContactTests : TestFixtureBase
+    {
+        /// <summary>
+        /// Contact format specified as Enum, but SDK represents it as a string.
+        /// Test checks that value parsing works properly
+        /// </summary>
+        [Test]
+        public async Task ContactFormatTest()
+        {
+            foreach (var format in new[] {"vcard", "msg"})
+            {
+                var extension = format == "vcard" ? ".vcf" : ".msg";
+                var name = $"{Guid.NewGuid().ToString()}{extension}";
+                await EmailApi.CreateContactAsync(new CreateContactRequest(format, name,
+                    new HierarchicalObjectRequest(
+                        new HierarchicalObject("CONTACT", null, new List<BaseObject>()),
+                        new StorageFolderLocation(StorageName, Folder))));
+                var contactExist = await IsFileExist(name);
+                Assert.IsTrue(contactExist);
+            }
+        }
+
+        [Test]
+        public async Task ContactModelTest()
+        {
+            var contact = new ContactDto
+            {
+                Gender = "Male",
+                Surname = "Thomas",
+                GivenName = "Alex",
+                EmailAddresses = new List<EmailAddress>
+                {
+                    new EmailAddress
+                    {
+                        Category = new EnumWithCustomOfEmailAddressCategory("Work", null),
+                        Address = "alex.thomas@work.com",
+                        Preferred = true,
+                        DisplayName = "Alex Thomas"
+                    }
+                },
+                PhoneNumbers = new List<PhoneNumber>
+                {
+                    new PhoneNumber
+                    {
+                        Category = new EnumWithCustomOfPhoneNumberCategory("Work", null),
+                        Number = "+49211424721",
+                        Preferred = true
+                    }
+                }
+            };
+
+            var contactFile = $"{Guid.NewGuid().ToString()}.vcf";
+            await EmailApi.SaveContactModelAsync(
+                new SaveContactModelRequest(
+                    "VCard", contactFile,
+                    new StorageModelRqOfContactDto(contact,
+                        new StorageFolderLocation(StorageName, Folder))));
+            var exists = await IsFileExist(contactFile);
+            Assert.True(exists);
+        }
+
+        [Test]
+        public async Task ConvertContactTest()
+        {
+            const string surname = "Cane";
+            var contactDto = new ContactDto
+            {
+                Gender = "Male",
+                Surname = surname,
+                GivenName = "John",
+                EmailAddresses = new List<EmailAddress>
+                {
+                    new EmailAddress {Address = "address@aspose.com"}
+                },
+                PhoneNumbers = new List<PhoneNumber>
+                {
+                    new PhoneNumber {Number = "+47235345346"}
+                }
+            };
+            var mapiStream = await EmailApi.ConvertContactModelToFileAsync(
+                new ConvertContactModelToFileRequest(
+                    "Msg", contactDto));
+            var vcardStream = await EmailApi.ConvertContactAsync(new ConvertContactRequest(
+                "VCard", "Msg", mapiStream));
+            using (var memoryStream = new MemoryStream())
+            {
+                await vcardStream.CopyToAsync(memoryStream);
+                var vcardString = Encoding.UTF8.GetString(memoryStream.ToArray());
+                Assert.IsTrue(vcardString.Contains(surname));
+            }
+
+            vcardStream.Seek(0, SeekOrigin.Begin);
+            var dto = await EmailApi.GetContactFileAsModelAsync(
+                new GetContactFileAsModelRequest("VCard", vcardStream));
+            Assert.AreEqual(surname, dto.Surname);
+        }
+    }
+}
---
+++ Aspose.Email-Cloud.Tests/Utils/TestFixtureBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Aspose.Email.Cloud.Sdk.Api;
+using Aspose.Email.Cloud.Sdk.Client;
+using Aspose.Email.Cloud.Sdk.Model.Requests;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace Aspose.Email.Cloud.Sdk.Tests.Utils
+{
+    [TestFixture]
+    public abstract class TestFixtureBase
+    {
+        protected const string StorageName = "First Storage";
+        protected EmailApi EmailApi;
+        protected string Folder;
+        /// <summary>
+        /// EmailApi setup.
+        /// Uses ConfigurationHelper to get appKey, appSid and apiBaseUrl.
+        /// This data can be configured using:
+        /// - NUnit test parameters (command line "--params" or from runsettings file
+        /// - Environment Variables
+        /// - Add fields to appsettings.json 
+        /// </summary>
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            Folder = Guid.NewGuid().ToString();
+            var configurationHelper = ConfigurationHelper.Get(TestContext.Parameters);
+            var apiConfiguration = new Configuration
+            {
+                ApiVersion = "v3.0",
+                ApiBaseUrl =
+                    configurationHelper.GetValue("apiBaseUrl", "https://api-qa.aspose.cloud"),
+                AppKey = configurationHelper.GetValue("appKey", string.Empty),
+                AppSid = configurationHelper.GetValue("appSid", string.Empty),
+                AuthUrl = configurationHelper.GetValue<string>("authUrl", null)
+            };
+            EmailApi = new EmailApi(apiConfiguration);
+        }
+        
+        [OneTimeTearDown]
+        public async Task OneTimeTearDown()
+        {
+            var existRequest = new ObjectExistsRequest(Folder, StorageName);
+            var folderExist = await EmailApi.ObjectExistsAsync(existRequest);
+            if (folderExist.Exists == true && folderExist.IsFolder == true)
+            {
+                var deleteRequest = new DeleteFolderRequest(Folder, StorageName, true);
+                await EmailApi.DeleteFolderAsync(deleteRequest);
+            }
+        }
+        protected static string FileToBase64(string filePath)
+        {
+            var bytes = File.ReadAllBytes(filePath);
+            return Convert.ToBase64String(bytes);
+        }
+        
+        protected async Task<bool> IsFileExist(string fileName, string folderPath = null)
+        {
+            var path = $"{folderPath ?? Folder}/{fileName}";
+            var request = new ObjectExistsRequest(path, StorageName);
+            var result = await EmailApi.ObjectExistsAsync(request);
+            return result.IsFolder != true && result.Exists == true;
+        }
+    }
+}
