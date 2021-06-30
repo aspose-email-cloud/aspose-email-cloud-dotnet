4ea73bbcea24e4e18df00a2757ed5c3d871b5ae5
ShakeelFaiz <shakeel.faiz@aspose.com>
2021-06-30 13:10:08 +0500

    Demos Application Ported to ASP.NET Core 5.0

---
+++ Demos/Aspose.Email.Cloud.Live.Demos/Controllers/ConversionController.cs
@@ -0,0 +1,64 @@
+﻿using Aspose.Email.Cloud.Live.Demos.Models;
+using Aspose.Email.Cloud.Live.Demos.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace Aspose.Email.Cloud.Live.Demos.Controllers
+{
+    public class ConversionController : BaseController
+    {
+        public ConversionController(IMemoryCache cache, IAsposeEmailCloudApiService emailService) : base(cache)
+        {
+            EmailService = emailService;
+        }
+
+        public override string Product => (string)RouteData.Values["product"];
+
+        public IAsposeEmailCloudApiService EmailService { get; }
+
+        public IActionResult Conversion()
+        {
+            var model = new ViewModel(this, nameof(Conversion))
+            {
+                SaveAsComponent = true,
+                MaximumUploadFiles = 1,
+                UseSorting = false
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<Response> Conversion(string outputType)
+        {
+            MemoryStream ms = new MemoryStream();
+
+            IFormFile postedFile = Request.Form.Files[0];
+            string fileName = postedFile.FileName;
+            await postedFile.CopyToAsync(ms);
+            ms.Position = 0;
+
+            string fromFormat = Path.GetExtension(fileName).Substring(1);
+            string toFormat = outputType;
+            var file = ms;
+            string outputFileName = Path.GetFileNameWithoutExtension(fileName) + "." + outputType;
+
+            MemoryStream convertResult = EmailService.Convert(file, fromFormat, toFormat) as MemoryStream;
+
+            string fileData = Convert.ToHexString(convertResult.ToArray());
+
+            return new Response
+            {
+                StatusCode = 200,
+                FileName = outputFileName,
+                FileData = fileData
+            };
+        }
+
+    }
+}
---
+++ Demos/Aspose.Email.Cloud.Live.Demos/Services/AsposeEmailCloudApiService.cs
@@ -0,0 +1,38 @@
+﻿using Aspose.Email.Cloud.Sdk.Api;
+using Aspose.Email.Cloud.Sdk.Model;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Aspose.Email.Cloud.Live.Demos.Services
+{
+    public interface IAsposeEmailCloudApiService
+    {
+        Stream Convert(Stream file, string fromFormat, string toFormat);
+    }
+
+    public class AsposeEmailCloudApiService : IAsposeEmailCloudApiService
+    {
+        public readonly EmailCloud EmailCloudApi;
+
+        public AsposeEmailCloudApiService(IConfiguration config)
+        {
+            string ClientId = config["AsposeEmailUserData:ClientId"];
+            string ClientSecret = config["AsposeEmailUserData:ClientSecret"];
+
+            EmailCloudApi = new EmailCloud(clientSecret: ClientSecret, clientId: ClientId);
+        }
+
+        public Stream Convert(Stream file, string fromFormat, string toFormat)
+        {
+            EmailConvertRequest ecr = new EmailConvertRequest()
+            {
+                File = file,
+                FromFormat = fromFormat,
+                ToFormat = toFormat
+            };
+
+            var convertResult = EmailCloudApi.Email.Convert(ecr);
+            return convertResult;
+        }
+    }
+}
