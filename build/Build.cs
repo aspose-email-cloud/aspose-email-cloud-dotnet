b583b275de26e6b7adeffda995a09e5a649505ad
Marat Gumerov <marat.gumerof@aspose.com>
2023-07-24 21:18:17 +0800

    EMAILCLOUD-465: Added deploy targets and corresponding workflow

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
+using JetBrains.Annotations;
 using Nuke.Common;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
@@ -17,27 +20,58 @@ class Build : NukeBuild
 
     public static int Main () => Execute<Build>(x => x.Test);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Parameter("API base URL")] public string ApiBaseUrl = "https://api.aspose.cloud";
 
-    [Parameter("API base URL")] string ApiBaseUrl = "https://api.aspose.cloud";
+    [Parameter("Client ID")] public string ClientId = "email.cloud";
+    [Parameter("Client Secret")] public string ClientSecret = "email.cloud";
+    
+    [Parameter("NuGet API key")] public string? NuGetApiKey;
 
-    [Parameter("Client ID")] string ClientId = "email.cloud";
-    [Parameter("Client Secret")] string ClientSecret = "email.cloud";
+    [Solution] public Solution Solution = null!;
 
-    [Solution] Solution Solution = null!;
+    AbsolutePath BuildOut => Solution.Directory / "build_out";
 
     Target Test => _ => _
         .Executes(() =>
         {
-            var testProject = Solution.GetProject("Aspose.Email-Cloud.Tests") ?? throw new Exception("Test project not found");
-            return DotNetTest(_ => _
+            var testProject = Solution.GetProject($"{Solution.Name}.Tests") ?? throw new Exception("Test project not found");
+            DotNetTest(__ => __
                 .SetProjectFile(testProject.Path)
                 .SetFilter("TestCategory=Pipeline")
                 .SetProcessWorkingDirectory(testProject.Directory)
+                .SetConfiguration(Configuration.Debug)
                 .SetProcessEnvironmentVariable("clientId", ClientId)
                 .SetProcessEnvironmentVariable("clientSecret", ClientSecret)
                 .SetProcessEnvironmentVariable("apiBaseUrl", ApiBaseUrl));
         });
 
+    Target Pack => _ => _
+        .DependsOn(Test)
+        .Executes(() => DotNetPack(__ => __
+            .SetProject(Solution.GetProject(Solution.Name))
+            .SetConfiguration(Configuration.Release)
+            .SetOutputDirectory(BuildOut)));
+
+    [UsedImplicitly]
+    Target Deploy => _ => _
+        .DependsOn(Pack)
+        .Requires(() => NuGetApiKey != null)
+        .Executes(() =>
+        {
+            var version = GetVersionString();
+            var package = BuildOut / $"{Solution.Name}.{version}.nupkg";
+            Assert.True(File.Exists(package), $"Package file with version {version} not found");
+            DotNetNuGetPush(__ => __
+                .SetApiKey(NuGetApiKey)
+                .SetSource("nuget.org")
+                .SetTargetPath(package));
+        });
+
+    string GetVersionString()
+    {
+        var project = Solution.GetProject(Solution.Name);
+        var version = new Version(project.GetProperty("Version") ?? $"{DateTime.Today.Year}.{DateTime.Today.Month}.0");
+        var fieldCount = version.Revision == 0 ? 3 : 4;
+        return version.ToString(fieldCount);
+    }
 }
--- build/Configuration.cs
+++ build/Configuration.cs
@@ -6,8 +6,8 @@ namespace Builder;
 [TypeConverter(typeof(TypeConverter<Configuration>))]
 public class Configuration : Enumeration
 {
-    public static Configuration Debug = new() { Value = nameof(Debug) };
-    public static Configuration Release = new() { Value = nameof(Release) };
+    public static readonly Configuration Debug = new() { Value = nameof(Debug) };
+    public static readonly Configuration Release = new() { Value = nameof(Release) };
 
     public static implicit operator string(Configuration configuration)
     {
